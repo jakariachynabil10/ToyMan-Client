ed0cdc33ed6a6f1b6ff2ba2c5f9b44278edc81c6
jakariachynabil10 <jakariachynabil@gmail.com>
2023-05-18 19:33:16 +0600

    shop by category is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,15 @@
-import { useState } from "react";
 import "./App.css";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import { useEffect } from "react";
+
 import Banner from "./Banner/Banner";
 import ShopByCategory from "./ShopByCategory/ShopByCategory";
 
 function App() {
-
-  // const [marvels, setMarvels] = useState([])
-  // const [avengers, setAvengers] = useState([])
-  // const [starWars, setStarWars] = useState([])
-  // useEffect( ()=>{
-  //   fetch('http://localhost:4300/categories')
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     console.log(data[0].Marvels)
-  //     setAvengers(data[0].Avengers)
-  //     setStarWars(data[0].StarWars)
-  //     setMarvels(data[0].Marvels)
-  //   })
-  // }, [])
-
-  // return (
-  //   <>
-  //     <Tabs>
-  //       <TabList>
-  //         <Tab>Marvels</Tab>
-  //         <Tab>Avengers</Tab>
-  //         <Tab>Star Wars</Tab>
-  //       </TabList>
-
-  //       <TabPanel>
-  //     {
-  //       marvels.map(m => console.log(m))
-  //     }
-  //       </TabPanel>
-  //       <TabPanel>
-  //         <h2>Any content 2</h2>
-  //       </TabPanel>
-  //       <TabPanel>
-  //         <h2>Any content 3</h2>
-  //       </TabPanel>
-  //     </Tabs>
-  //   </>
-  // );
-
-  return <>
-     <Banner></Banner>
-     <ShopByCategory></ShopByCategory>
-  </>
+  return (
+    <>
+      <Banner></Banner>
+      <ShopByCategory></ShopByCategory>
+    </>
+  );
 }
 
 export default App;
--- src/ShopByCategory/ShopByCategory.jsx
+++ src/ShopByCategory/ShopByCategory.jsx
@@ -4,18 +4,19 @@ import { useEffect } from "react";
 import SingleCategory from "./SingleCategory";
 
 const ShopByCategory = () => {
-  const [marvels, setMarvels] = useState([]);
-  const [avengers, setAvengers] = useState([]);
-  const [starWars, setStarWars] = useState([]);
+  const [toys, setToys] = useState([]);
+  const [category, setCategory] = useState("Marvel Toys");
+ 
+
+  const url = `http://localhost:4300/toys?sub_category=${category}`;
   useEffect(() => {
-    fetch("http://localhost:4300/categories")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        setAvengers(data[0].Avengers);
-        setStarWars(data[0].StarWars);
-        setMarvels(data[0].Marvels);
+        setToys(data);
+        console.log(data);
       });
-  }, []);
+  }, [category]);
 
   return (
     <>
@@ -24,30 +25,87 @@ const ShopByCategory = () => {
       </h1>
       <Tabs>
         <TabList>
-          <Tab>Marvels</Tab>
-          <Tab>Avengers</Tab>
-          <Tab>Star Wars</Tab>
+          <Tab onClick={() => setCategory("Marvel Toys")}>Marvels</Tab>
+          <Tab onClick={() => setCategory("Avengers Toys")}>Avengers</Tab>
+          <Tab onClick={() => setCategory("Star Wars Toys")}>Star Wars</Tab>
         </TabList>
 
         <TabPanel>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-16 lg:mx-4 mt-10 font-serif">
-            {marvels.map((marvel) => (
-              <SingleCategory marvel={marvel}></SingleCategory>
-            ))}
+            {toys.map((toy) => {
+              const { name, picture, rating, price } = toy;
+              return (
+                <>
+                  <div className="card card-compact w-[90%]  shadow-xl">
+                    <figure>
+                      <img
+                        src={picture}
+                        alt="Shoes"
+                      />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </TabPanel>
         <TabPanel>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-16 lg:mx-4 mt-10 font-serif">
-            {avengers.map((marvel) => (
-              <SingleCategory marvel={marvel}></SingleCategory>
-            ))}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-16 lg:mx-4 mt-10 font-serif">
+          {toys.map((toy) => {
+              const { name, picture, rating, price } = toy;
+              return (
+                <>
+                  <div className="card card-compact w-[90%]  shadow-xl">
+                    <figure>
+                      <img
+                        src={picture}
+                        alt="Shoes"
+                      />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </TabPanel>
         <TabPanel>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-16 lg:mx-4 mt-10 font-serif">
-            {starWars.map((marvel) => (
-              <SingleCategory marvel={marvel}></SingleCategory>
-            ))}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 mx-16 lg:mx-4 mt-10 font-serif">
+          {toys.map((toy) => {
+              const { name, picture, rating, price } = toy;
+              return (
+                <>
+                  <div className="card card-compact w-[90%]  shadow-xl">
+                    <figure>
+                      <img
+                        src={picture}
+                        className=""
+                      />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </TabPanel>
       </Tabs>
