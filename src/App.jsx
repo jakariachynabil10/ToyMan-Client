629703bca192a074fefa7ac8e3f99b28bb485129
jakariachynabil10 <jakariachynabil@gmail.com>
2023-05-18 10:38:07 +0600

    navbar is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { useEffect } from "react";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
- 
 
-  return (
-    <>
-      
-    </>
-  )
+  // const [marvels, setMarvels] = useState([])
+  // const [avengers, setAvengers] = useState([])
+  // const [starWars, setStarWars] = useState([])
+  // useEffect( ()=>{
+  //   fetch('http://localhost:4300/categories')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data[0].Marvels)
+  //     setAvengers(data[0].Avengers)
+  //     setStarWars(data[0].StarWars)
+  //     setMarvels(data[0].Marvels)
+  //   })
+  // }, [])
+
+  // return (
+  //   <>
+  //     <Tabs>
+  //       <TabList>
+  //         <Tab>Marvels</Tab>
+  //         <Tab>Avengers</Tab>
+  //         <Tab>Star Wars</Tab>
+  //       </TabList>
+
+  //       <TabPanel>
+  //     {
+  //       marvels.map(m => console.log(m))
+  //     }
+  //       </TabPanel>
+  //       <TabPanel>
+  //         <h2>Any content 2</h2>
+  //       </TabPanel>
+  //       <TabPanel>
+  //         <h2>Any content 3</h2>
+  //       </TabPanel>
+  //     </Tabs>
+  //   </>
+  // );
+
+  return <>
+      <div className="mx-10">
+      <Navbar></Navbar>
+      </div>
+  </>
 }
 
-export default App
+export default App;
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const items = (
+    <>
+      <li className="hover:text-blue-700 font-bold">
+        <Link to="/">Home</Link>
+      </li>
+
+      <li className="hover:text-blue-700 font-bold">
+        <Link to="/allToys">All toys</Link>
+      </li>
+
+      <li className="hover:text-blue-700 font-bold">
+        <Link to="/myToys">My toys</Link>
+      </li>
+
+      <li className="hover:text-blue-700 font-bold">
+        <Link to="/addToys">Add toys</Link>
+      </li>
+
+      <li className="hover:text-blue-700 font-bold">
+        <Link to="/blog">Blog</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu  menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {items}
+            </ul>
+          </div>
+         <Link to='/' className="flex items-center gap-1 text-4xl font-bold"> <img src="https://i.ibb.co/yg12D0d/toyman-2.webp" className="w-[50px]" alt="" /> <span className="text-blue-800">Toy</span> <span className="text-pink-600">Man</span></Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu   font-serif menu-horizontal px-1">{items}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link
+            to="/login"
+            className="border px-7 py-2 font-serif border-rose-400 hover:bg-rose-400 text-black hover:text-white font-semibold"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
